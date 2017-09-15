2a893dbcf0affeb5fefff9bb1a8b67d971eabb50
fenyouse <l.lescene@gmail.com>
2017-09-15 09:33:15 +0200

    Intégration $MonBeauSI

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
-include_once('Vues/header.php');
+  require_once 'Vues/header.php';
+
+	//référencer les classes utiles
+	require_once 'Modeles/si.php';
+	//recupération du SI, le seul le singleton
+	$MonBeauSI = SI::getSI();
+	var_dump($MonBeauSI);//montrer l'id de l'objet
 ?>
 
-<h1>Hello, world!</h1>
-
-
+  <h1>Hello, world!</h1>
 
 
 <?php
-include_once('Vues/footer.php');
+  require_once 'Vues/footer.php';
 ?>
