26211206850c81b3b6049637d017e80572b8c81f
Mariesc <31404263+Mariesc@users.noreply.github.com>
2017-11-10 19:18:42 +0100

    Revert "Revert "Ajout de la vue du technicien""
    This reverts commit 5206c55739a654f5d6f4f2da4defcbd1f6f97ac4.

---
+++ Modeles/importlisteEleve.php
@@ -0,0 +1,72 @@
+<?php
+extract(filter_input_array(INPUT_POST));
+
+$db = new PDO('mysql:host=127.0.0.1;dbname=cvl','root','');
+$fichier=$_FILES["userfile"]["name"];
+$type_file = pathinfo($fichier,PATHINFO_EXTENSION);
+
+if($type_file =="csv"){
+	if($fichier){ // ouverture du fichier temporaire
+		$fp = fopen($_FILES["userfile"]["tmp_name"], "r");
+	}
+	else{ //fichier inconnu
+		//message d'erreur
+		$message = utf8_decode("- Importation Echouée - ");?>
+		 <p align="center" > <?php echo '<script type="text/javascript">window.alert("'.$message.'");</script>'?> </p>
+		 <p align="center" ><B>Désolé, mais vous n'avez pas specifié de chemin valide...</B></p>
+	 <?php exit();}
+	//declaration de la variable cpt qui permettre de compter le nomde d'enregistrement réalisé
+	$cpt=0;
+	//message après traitement
+	$message = utf8_decode("- Importation Réussie - ");?>
+	<p align="center"><?php echo '<script type="text/javascript">window.alert("'.$message.'");</script>' ?></p>
+	<?php //importation
+	// ignore ligne 1
+	$ignore = true ;
+	while(!feof($fp)){
+		$ligne =fgets($fp,4096);
+		
+		if (!$ignore) {
+			//oncrée un tableau des éléments séparés pas des points virgule
+			$ligne = explode(";",$ligne);
+			$table = filter_input(INPUT_POST,'userfile');
+			//permier éléments
+			$liste[0] = (isset($ligne[0]) ) ? $ligne[0] : Null;
+			$liste[1] = (isset($ligne[1]) ) ? $ligne[1] : Null;
+			$liste[2] = (isset($ligne[2]) ) ? $ligne[2] : Null;
+			$liste[3] = (isset($ligne[3]) ) ? $ligne[3] : Null;
+			$liste[4] = (isset($ligne[4]) ) ? $ligne[4] : Null;
+			$liste[5] = (isset($ligne[5]) ) ? $ligne[5] : Null;
+			$liste[6] = (isset($ligne[6]) ) ? $ligne[6] : Null;
+			$liste[7] = (isset($ligne[7]) ) ? $ligne[7] : Null;
+			$champ1=$liste[0];
+			$champ2=$liste[1];
+			$champ3=$liste[2];
+			$champ4=$liste[3];
+			$champ5=$liste[4];
+			$champ6=$liste[5];
+			$champ7=$liste[6];
+			$champ8=$liste[7];
+			if($champ1!='')
+			{
+				$cpt++;
+				$req =("INSERT INTO elect( EId,ENom,EPrenom,ECodeINE,EPwd,ELogin,EIdDivis) VALUES('$champ2','$champ4','$champ5','$champ3','$champ8','$champ7','$champ1')");
+				$result = $db-> query($req);
+			}
+			
+		
+		}
+		$ignore=false;
+		
+	}
+}
+else{
+	exit("Attention ! Le fichier n'est pas en csv, veuillez en importer un nouveau !");
+}
+	
+
+//fermeture du fichier
+	fclose($fp);
+	
+	?>
+	
---
+++ Vues/Tech.php
@@ -0,0 +1,34 @@
+﻿<?php  
+// masque les messages erreurs
+ini_set("display_errors",0);error_reporting(0);
+//$MonBeauSI = SI::getSI();
+?>
+<html lang="fr">
+	<head>
+	<meta charset="utf-8" />
+	<title>Compte Technicien</title>
+</head>
+<body>
+<center>
+<h3> Veuillez choisir un fichier *.csv : </h3>
+<form method="post" enctype="multipart/form-data" action="../Modeles/importlisteEleve.php">
+<input name="userfile" type="file" value="table" />
+<input name="submit" type="submit" value="Importer" /></form>
+
+<h3>Exportation des Résultats du suffrage : </h3>
+<!--<p>Selection du suffrage :</p>-->
+<?php
+// require_once '../Modeles/suffrage.php';
+// $lesSuffrages = new Suffrages();
+// $lesSuffrages ->remplir();
+// Suffrage::getInstances()->displaySelect();
+?>
+<form method="post" action="../Modeles/exportationResultats.php">
+<input name="submit" type="submit" value="Exporter" /></form>
+
+<h3>Exportation des Login des étudiants : </h3>
+<form method="post" action="../Modeles/exportationLogin.php">
+<input name="submit" type="submit" value="Exporter" /></form>
+</center>
+</body>
+</html>
