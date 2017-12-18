7b5151643e08d594d634ecb10e1a9526347f7cf1
Mariesc <31404263+Mariesc@users.noreply.github.com>
2017-12-18 18:13:20 +0100

    modif des chemins pour index

--- Vues/Tech.php
+++ Vues/Tech.php
@@ -11,18 +11,18 @@ ini_set("display_errors",0);error_reporting(0);
 <h4>Importation de la Liste des élèves : </h4>
 <p> Veuillez choisir un fichier *.csv : </p>
 
-<form  method="post" enctype="multipart/form-data" action="../index.php">
+<form  method="post" enctype="multipart/form-data" action="index.php">
 <input style="padding-bottom:40px" class="form-control" name="userfile" type="file" value="table" />
 <input class="btn btn-default btn-lg btn-block" name="Importer" type="submit" value="Importer" /></form>
 <br></br>
 
 <h4>Exportation des Résultats du suffrage : </h4>
-<form  method="post" action="../index.php">
+<form  method="post" action="index.php">
 <input class="btn btn-default btn-lg btn-block"  name="submitRes" type="submit" value="Exporter" /></form>
 <br></br>
 
 <h4>Exportation des Login des étudiants : </h4>
-<form  method="post" action="../index.php">
+<form  method="post" action="index.php">
 <input class="btn btn-default btn-lg btn-block" name="submitLog" type="submit" value="Exporter" /></form>
 <br></br>
 
