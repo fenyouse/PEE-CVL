a262e7e1ab185c918e12364daece04486b422669
Fenyouse <8859822+fenyouse@users.noreply.github.com>
2017-12-22 09:44:26 +0100

    indentation fichier

--- Vues/Tech.php
+++ Vues/Tech.php
@@ -6,100 +6,113 @@ ini_set("display_errors",0);error_reporting(0);
 
 
 <center>
-<div class="well center-block" style="max-width:400px;margin-top:100px">
+  <div class="well center-block" style="max-width:400px;margin-top:100px">
 
-<h4>Importation de la Liste des élèves : </h4>
-<p> Veuillez choisir un fichier *.csv : </p>
- 
-<form  method="post" enctype="multipart/form-data" action="index.php">
-<input style="padding-bottom:40px" class="form-control" name="userfile" type="file" value="table" />
-<input class="btn btn-default btn-lg btn-block" name="Importer" type="submit" value="Importer" /></form>
-<br></br>
+    <h4>Importation de la Liste des élèves : </h4>
+    <p> Veuillez choisir un fichier *.csv : </p>
 
-<h4>Exportation des Résultats du suffrage : </h4>
-<form  method="post" action="index.php">
-	<p>Suffrage :
-<?php
-	$LPD = new Suffrages();
-	$LPD->remplir();
-	Suffrage::getInstances()->displaySelectSimple("Suffrage");
-?></p>
-<input class="btn btn-default btn-lg btn-block"  name="submitRes" type="submit" value="Exporter" /></form>
-<br></br>
+    <form  method="post" enctype="multipart/form-data" action="index.php">
+      <input style="padding-bottom:40px" class="form-control" name="userfile" type="file" value="table" />
+      <input class="btn btn-default btn-lg btn-block" name="Importer" type="submit" value="Importer" />
+    </form>
+    </br>
+    </br>
 
-<h4>Exportation des Login des étudiants : </h4>
-<form  method="post" action="index.php">
-<input class="btn btn-default btn-lg btn-block" name="submitLog" type="submit" value="Exporter" /></form>
-<br></br>
+    <h4>Exportation des Résultats du suffrage : </h4>
+    <form  method="post" action="index.php">
+    	<p>Suffrage :
+    <?php
+    	$LPD = new Suffrages();
+    	$LPD->remplir();
+    	Suffrage::getInstances()->displaySelectSimple("Suffrage");
+    ?></p>
+      <input class="btn btn-default btn-lg btn-block"  name="submitRes" type="submit" value="Exporter" />
+    </form>
+    </br>
+    </br>
 
-<!-- tableau affiche les divisions-->
-<?php
-	$LPD = new Divisions();
-	$LPD->remplir();
-	Division::getInstances()->displayTable("Divis2");
-?>
-<h4>Nouvelle division :</h4>
-<form class="form-inline" method="post" action="index.php">
-	<div class="form-group">
-      <label class="sr-only" >Division</label>
-      <input type="text" class="form-control" name="divisAjout" id="divisAjout"  placeholder="Division">
-    </div>
-	<br></br>
-	<input class="btn btn-primary btn-lg btn-block" type="submit" value="Valider" name="ValiderDivis"class="bouton" />
-<br></br></form>
+    <h4>Exportation des Login des étudiants : </h4>
+    <form  method="post" action="index.php">
+      <input class="btn btn-default btn-lg btn-block" name="submitLog" type="submit" value="Exporter" />
+    </form>
+    </br>
+    </br>
 
-<h4>Suppression d'une division :</h4>
-<form class="form-inline" method="post" action="index.php">
-<p>Division :
-<?php
-	$LPD = new Divisions();
-	$LPD->remplir();
-	Division::getInstances()->displaySelect("Divis2");
-?></p>
-	<input class="btn btn-primary btn-lg btn-block" type="submit" value="Supprimer" name="ValiderSupp"class="bouton" />
-<br></br></form>
+    <!-- tableau affiche les divisions-->
+    <?php
+    	$LPD = new Divisions();
+    	$LPD->remplir();
+    	Division::getInstances()->displayTable("Divis2");
+    ?>
+    <h4>Nouvelle division :</h4>
+    <form class="form-inline" method="post" action="index.php">
+    	<div class="form-group">
+          <label class="sr-only">Division</label>
+          <input type="text" class="form-control" name="divisAjout" id="divisAjout"  placeholder="Division">
+        </div>
+    	</br>
+      </br>
+    	<input class="btn btn-primary btn-lg btn-block" type="submit" value="Valider" name="ValiderDivis"class="bouton" />
+      </br>
+      </br>
+    </form>
+
+    <h4>Suppression d'une division :</h4>
+    <form class="form-inline" method="post" action="index.php">
+      <p>Division :
+    <?php
+    	$LPD = new Divisions();
+    	$LPD->remplir();
+    	Division::getInstances()->displaySelect("Divis2");
+    ?></p>
+    	<input class="btn btn-primary btn-lg btn-block" type="submit" value="Supprimer" name="ValiderSupp"class="bouton" />
+      </br>
+      </br>
+    </form>
 
 
-<h4>Nouvel élève :</h4>
-<form class="form-inline" method="post" action="index.php">
+    <h4>Nouvel élève :</h4>
+    <form class="form-inline" method="post" action="index.php">
 
-	<div class="form-group">
-      <label class="sr-only" >EId</label>
-      <input type="text" class="form-control" name="EId" id="EId"  placeholder="EId">
-    </div>
-	<div class="form-group">
-      <label class="sr-only" >Nom</label>
-      <input type="text" class="form-control" name="Nom" id="Nom"  placeholder="Nom">
-    </div>
-	<div class="form-group">
-      <label  class="sr-only" >INE</label>
-      <input type="text" class="form-control" name="INE" id="INE" placeholder="INE">
-    </div>
-	<div class="form-group">
-      <label  class="sr-only">Prenom</label>
-      <input type="text" class="form-control" name="Prenom" id="Prenom" placeholder="Prenom">
-    </div>
-	<div class="form-group">
-      <label class="sr-only">Login</label>
-      <input type="text" class="form-control" name="Login" id="login" placeholder="Login">
-    </div>
-    <div class="form-group">
-      <label class="sr-only" for="InputPassword">Mot de passe</label>
-      <input name="mdp" type="password" id="mdp" class="form-control" placeholder="Mot de passe">
-	</div>
-<br></br>
-	<p>Division :
-<?php
-	$LPD = new Divisions();
-	$LPD->remplir();
-	Division::getInstances()->displaySelect("Divis");
-?></p>
-<br></br>
+    	<div class="form-group">
+          <label class="sr-only" >EId</label>
+          <input type="text" class="form-control" name="EId" id="EId"  placeholder="EId">
+        </div>
+    	<div class="form-group">
+          <label class="sr-only" >Nom</label>
+          <input type="text" class="form-control" name="Nom" id="Nom"  placeholder="Nom">
+        </div>
+    	<div class="form-group">
+          <label  class="sr-only" >INE</label>
+          <input type="text" class="form-control" name="INE" id="INE" placeholder="INE">
+        </div>
+    	<div class="form-group">
+          <label  class="sr-only">Prenom</label>
+          <input type="text" class="form-control" name="Prenom" id="Prenom" placeholder="Prenom">
+        </div>
+    	<div class="form-group">
+          <label class="sr-only">Login</label>
+          <input type="text" class="form-control" name="Login" id="login" placeholder="Login">
+      </div>
+      <div class="form-group">
+        <label class="sr-only" for="InputPassword">Mot de passe</label>
+        <input name="mdp" type="password" id="mdp" class="form-control" placeholder="Mot de passe">
+      </div>
+      </br>
+      </br>
+    	<p>Division :
+    <?php
+    	$LPD = new Divisions();
+    	$LPD->remplir();
+    	Division::getInstances()->displaySelect("Divis");
+    ?></p>
+      </br>
+      </br>
 
-	<input class="btn btn-primary btn-lg btn-block" type="submit" value="Valider" name="Valider"class="bouton" />
-</form>
+      <input class="btn btn-primary btn-lg btn-block" type="submit" value="Valider" name="Valider"class="bouton" />
+    </form>
+    </br>
+    </br>
 
- <br></br>
-
-</div>
+  </div>
 </center>
